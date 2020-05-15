a70efd35da10c7f7029a9e9553d3b8aad261c215
jakjare <jakjare@wp.pl>
2020-05-15 15:14:58 +0200

    Dodanie listy wyboru biletów.
    Aktualnie dodałem listę wyboru biletów. Algorytm pobiera wprowadzone wcześniej do automatu bilety i wyświetla je w dwóch kolumnach razem z przyciskami "+" oraz "-". Docelowo przyciski będą dodawać bilety do koszyka. Na razie ładnie wyglądają i podświetlają się przy najechaniu na nie kursorem. :D W pierwszej kolumnie wyświetlają się wszystkie bilety normalne a w drugiej ulgowe. Liczba biletów każdego rodzaju nie musi być taka sama ale tak czy siak będą wyświetlane w odpowiednich kolumnach.

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import time
 
 def pobierz_czas(label):
     czas = time.localtime()
-    label.config(text=time.strftime("%d/%m/%Y, %H:%M:%S", czas))
+    label.config(text=time.strftime("%d/%m/%Y %H:%M:%S", czas))
     def aktualizuj():
         czas = time.localtime()
-        label.config(text=time.strftime("%d/%m/%Y, %H:%M:%S", czas))
+        label.config(text=time.strftime("%d/%m/%Y %H:%M:%S", czas))
         label.after(1000, aktualizuj)
     aktualizuj()
+
+def on_enter(event):
+    event.widget.configure(bg="#9e9e9e")
+
+def on_leave(event):
+    event.widget.configure(bg="#646464")
+
 
 automat = system.System()                                   #Tworzę obiek automatu MPK
 bilety = [bilety.Bilety("Jednorazowy", "normalny", 3),
@@ -39,14 +46,14 @@ for i in range(30):
 automat.admin_kasa(kasa_automatu)                           #Dodaję pieniądze do kasy w automacie
 del kasa_automatu
 
-okno_glowne = Tk()
+okno_glowne = Tk()                                          #Tworzę okno główne aplikacji
 okno_glowne.title("Automat biletowy MPK")
-okno_glowne.geometry("1100x800")
-okno_glowne.resizable(False, False)
+okno_glowne.geometry("1100x900")
+okno_glowne.resizable(False, True)
 
 # Wyświetla nagłówek interfejsu
 naglowek = Frame(okno_glowne, bg = "#00a2ff")
-naglowek.pack(side = TOP)
+naglowek.pack(side = TOP, fill=X)
 o_logo = PhotoImage(file="C:/Users/jakja/PycharmProjects/projekt_js_automat_MPK\logo.png", width = 80)
 logo = Label(naglowek, bg="#00a2ff", fg="white", image = o_logo)
 logo.pack(side = LEFT, pady = 10, padx = 10)
@@ -56,8 +63,32 @@ czas_naglowek = Label(naglowek, width = 17, bg="#00a2ff", fg="white", font="Aria
 czas_naglowek.pack(side = TOP, pady = 5)
 pobierz_czas(czas_naglowek)
 
-# Wyświetla bilety
+# Wyświetla listę wyboru biletów
+ramka_bilety = Frame(okno_glowne)
+ramka_bilety.pack(side = TOP, fill=X, pady=10)
+lista_biletow = automat.bilety()
+i = 0
+j = 0
+for rodzaj in lista_biletow:
+    for bilet in lista_biletow[rodzaj]:
+        nazwa = "{} - {}\n\n{:.2f} zł".format(bilet.nazwa(), rodzaj, bilet.cena()/100)
+        b = Label(ramka_bilety, bg="#061981", text=nazwa, width=29, height=4,
+                  font="Arial 15", fg="white").grid(row=i, column=j, padx=(18, 3), pady=2)
+        b = Label(ramka_bilety, bg="#646464", text="+", width=8, height=4, font="Arial 15", fg="white")
+        b.bind("<Enter>", on_enter)
+        b.bind("<Leave>", on_leave)
 
+        b.grid(row=i, column=j+1, padx=2, pady=2)
+        b = Label(ramka_bilety, bg="#646464", text="-", width=8, height=4, font="Arial 15", fg="white")
+        b.bind("<Enter>", on_enter)
+        b.bind("<Leave>", on_leave)
+        b.grid(row=i, column=j+2, padx=2, pady=2)
+        i += 1
+    j += 3
+    i = 0
+del i, j, lista_biletow
+
+#Wyświetla koszyk, czyli listę aktualnie dodanych biletów oraz sumę do zapłaty
 
 
 
